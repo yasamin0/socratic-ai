2249e492801c4e801e44c2d1dc8f73022833a2c6
yasamin0 <81822900+yasamin0@users.noreply.github.com>
2025-06-08 19:25:25 +0200

    UPDATE MAIN

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ import pickle
 from datetime import datetime
 from fastapi.responses import FileResponse
 
+import spacy
+import subprocess
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+    nlp = spacy.load("en_core_web_sm")
 
 # Load spaCy model
 nlp = spacy.load("en_core_web_sm")
